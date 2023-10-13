d3973af981ad6709ae11008bd1fee6d53ffcc49c
Adeiza321 <sanniahmed908@gmail.com>
2023-10-14 00:43:11 +0100

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,8 @@
 #include "main.h"
+
 /**
  * more_numbers -print numbers from zero to fourteen 10 times
+ *
  * Return: void
  */
 
@@ -10,13 +12,13 @@ void more_numbers(void)
 int i, j;
 
 	for (i = 0; i <= 10; i++)
-	{
-	for (j = 0; j <= 14; j++)
-	{
-	if (j >= 10)
-	_putchar('1');
-	_putchar(j % 10 + '0');
-	}
+		{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
 	_putchar('\n');
 	}
 }
