2b966c71f7998a95b30fe88dd04cd11395bd7233
Adeiza321 <sanniahmed908@gmail.com>
2023-10-13 22:17:13 +0100

    my commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - print all numbers except 2 and 4
- * Return: Always 0
+ * Return: Always (0)
  */
 
 void print_most_numbers(void)
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 {
 	char n;
 
-	for (n ='0'; n <= '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 	if ((n == '2') || (n == '4'))
-		continue;
+	continue;
 
 	_putchar(n);
 
