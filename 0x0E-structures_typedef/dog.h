6aa5857a4546cf64f0aafdc8746ab54eb49e6d94
Adeiza321 <sanniahmed908@gmail.com>
2023-11-06 22:56:44 +0100

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,12 +3,12 @@
 
 
 /**
- * struct dog - Define a new type struct dog
- * @name: First member
- * @owner: Second member
- * @age: Third member
+ * struct dog - Giving dog attribbutes
+ * @name: The name
+ * @owner: The owner
+ * @age: The age
  *
- * Description: Define a new type struct dog
+ * Description: They attributes of a dog
  */
 
 struct dog
@@ -18,15 +18,6 @@ struct dog
 	char *owner;
 
 };
-/**
- * dog_t - Typedef for dog structure
- */
 
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
