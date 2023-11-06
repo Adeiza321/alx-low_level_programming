79c00238114e8e55d77b7abefdd1f30af8eebf0a
Adeiza321 <sanniahmed908@gmail.com>
2023-11-06 20:38:42 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef HEADER
+#define HEADER
+
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ *
+ * Description: Define a new type struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+/**
+ * dog_t - Typedef for dog structure
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
