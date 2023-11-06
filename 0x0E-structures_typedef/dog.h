742ce85964d812b19d1e5f783bbb0accf8aae41d
Adeiza321 <sanniahmed908@gmail.com>
2023-11-06 23:56:29 +0100

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,19 +11,14 @@
  * Description: They attributes of a dog
  */
 
-/**
- * dog_t - new namw for struct dog
- */
 
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
