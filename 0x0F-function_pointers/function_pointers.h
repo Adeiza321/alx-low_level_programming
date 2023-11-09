1c713ba4365861178187105f7af17a2ca6388667
Adeiza321 <sanniahmed908@gmail.com>
2023-11-09 02:10:08 +0100

    my commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef _FUNCTIONS_POINTERS
 #define _FUNCTION_POINTERS
+#include <stddef.h>
 
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
